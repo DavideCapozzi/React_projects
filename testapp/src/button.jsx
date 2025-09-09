59cfb807d443be682dfd904952682c95d8aaee44
DavideCapozzi <capozzi.davide99@gmail.com>
2025-09-09 19:50:36 +0200

    Finished circle button

--- testapp/src/button.jsx
+++ testapp/src/button.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { useState } from "react";
 import "./Button.css";
 
 function Button() {
+    const [isGray, setIsGray] = useState(true);
+
+    const handleClick = () => {
+        setIsGray(!isGray);
+    };
+
     return (
-        <div className="Button">
-            Hello 
+        <div className="circle-container">
+            <div className={`Button ${isGray ? '' : 'Button-red'}`} 
+                onClick={handleClick} >
+                Click me 
+            </div>
         </div>
     );
 }
